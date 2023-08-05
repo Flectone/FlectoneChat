ed575e40adb9d7b0dc68c4028247c0c61f9b09f8
TheFaser <artem.mochalin11@gmail.com>
2023-08-06 01:14:18 +0500

    Integrations: add DiscordSRV license.head

--- src/main/java/net/flectone/integrations/discordsrv/FDiscordSRV.java
+++ src/main/java/net/flectone/integrations/discordsrv/FDiscordSRV.java
@@ -8,6 +8,7 @@ import github.scarsz.discordsrv.api.events.GameChatMessagePreProcessEvent;
 import github.scarsz.discordsrv.dependencies.jda.api.EmbedBuilder;
 import github.scarsz.discordsrv.dependencies.jda.api.MessageBuilder;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.Message;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.MessageEmbed;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
 import github.scarsz.discordsrv.objects.MessageFormat;
 import github.scarsz.discordsrv.util.DiscordUtil;
@@ -16,6 +17,7 @@ import github.scarsz.discordsrv.util.PlaceholderUtil;
 import github.scarsz.discordsrv.util.TimeUtil;
 import net.flectone.Main;
 import net.flectone.utils.ObjectUtil;
+import net.flectone.custom.FAdvancement;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -25,6 +27,29 @@ import org.bukkit.inventory.ItemStack;
 
 import java.awt.*;
 import java.util.function.BiFunction;
+
+/*
+
+DiscordSRV - https://github.com/DiscordSRV/DiscordSRV
+
+Copyright (C) 2016 - 2022 Austin "Scarsz" Shapiro
+This file is part of DiscordDRV
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program.  If not, see
+<http://www.gnu.org/licenses/gpl-3.0.html>.
+
+ */
 
 public class FDiscordSRV implements Listener {
 
@@ -37,6 +62,17 @@ public class FDiscordSRV implements Listener {
     public static void register(){
         FDiscordSRV fDiscordSRV = new FDiscordSRV();
         DiscordSRV.api.subscribe(fDiscordSRV);
+    }
+
+    @Subscribe(priority = ListenerPriority.HIGHEST)
+    public void onChatMessageFromInGame(GameChatMessagePreProcessEvent event) { // From in-game to Discord
+        Player player = event.getPlayer();
+
+        String message = event.getMessage();
+
+        message = ObjectUtil.buildFormattedMessage(player, "discordchat", message, player.getInventory().getItemInMainHand());
+
+        event.setMessage(message);
     }
 
     public static void sendDeathMessage(Player player, String message, Entity finalEntity, Material finalBlock, Entity killer, ItemStack killerItem){
@@ -65,39 +101,15 @@ public class FDiscordSRV implements Listener {
             message = message.replace("<by_item>", byItemMessage.replace("<item>", itemName));
         }
 
-        message = ObjectUtil.formatString(message, null)
+        String deathMessage = ObjectUtil.formatString(message, null)
                 .replace("<killer>", "")
                 .replace("<projectile>", "")
                 .replace("<block>", "")
                 .replace("<due_to>", "")
                 .replace("<by_item>", "");
 
-        sendAvatarMessage(message, player, "deaths");
-    }
 
-    public static void sendModerationMessage(String message){
-        if(!isEnable) return;
-
-        message = ObjectUtil.formatString(message, null);
-        message = PlaceholderUtil.replacePlaceholdersToDiscord(message);
-
-        String channelName = DiscordSRV.getPlugin().getOptionalChannel("global");
-        TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName(channelName);
-
-        Message messageFormat = new MessageBuilder()
-                .setEmbeds(new EmbedBuilder().setColor(Color.RED).setDescription(message).build())
-                .build();
-
-        DiscordUtil.queueMessage(textChannel, messageFormat, true);
-    }
-
-    private static void sendAvatarMessage(String deathMessage, Player player){
-        sendAvatarMessage(deathMessage, player, "global");
-    }
-
-    private static void sendAvatarMessage(String deathMessage, Player player, String optionalChannel) {
-
-        String channelName = DiscordSRV.getPlugin().getOptionalChannel(optionalChannel);
+        String channelName = DiscordSRV.getPlugin().getOptionalChannel("deaths");
         MessageFormat messageFormat = DiscordSRV.getPlugin().getMessageFromConfiguration("MinecraftPlayerDeathMessage");
         if (messageFormat == null) return;
 
@@ -138,14 +150,77 @@ public class FDiscordSRV implements Listener {
         DiscordUtil.queueMessage(textChannel, discordMessage, true);
     }
 
-    @Subscribe(priority = ListenerPriority.HIGHEST)
-    public void onChatMessageFromInGame(GameChatMessagePreProcessEvent event) { // From in-game to Discord
-        Player player = event.getPlayer();
+    public static void sendModerationMessage(String message){
+        if(!isEnable) return;
 
-        String message = event.getMessage();
+        message = ObjectUtil.formatString(message, null);
+        message = PlaceholderUtil.replacePlaceholdersToDiscord(message);
 
-        message = ObjectUtil.buildFormattedMessage(player, "discordchat", message, player.getInventory().getItemInMainHand());
+        String channelName = DiscordSRV.getPlugin().getOptionalChannel("global");
+        TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName(channelName);
 
-        event.setMessage(message);
+        Message messageFormat = new MessageBuilder()
+                .setEmbeds(new EmbedBuilder().setColor(Color.RED).setDescription(message).build())
+                .build();
+
+        DiscordUtil.queueMessage(textChannel, messageFormat, true);
     }
+
+    public static void sendAdvancementMessage(Player player, FAdvancement fAdvancement, String lastAdvancement) {
+        if(!isEnable) return;
+
+        String channelName = DiscordSRV.getPlugin().getOptionalChannel("awards");
+
+        MessageFormat messageFormat = DiscordSRV.getPlugin().getMessageFromConfiguration("MinecraftPlayerAchievementMessage");
+        if (messageFormat == null) return;
+
+        String advancementTitle = fAdvancement.getTitle();
+
+        lastAdvancement = ObjectUtil.formatString(lastAdvancement, null)
+                .replace("<player>", player.getName())
+                .replace("<advancement>", advancementTitle);
+        lastAdvancement = PlaceholderUtil.replacePlaceholdersToDiscord(lastAdvancement);
+
+        String finalAchievementName = StringUtils.isNotBlank(advancementTitle) ? advancementTitle : "";
+        String avatarUrl = DiscordSRV.getAvatarUrl(player);
+        String botAvatarUrl = DiscordUtil.getJda().getSelfUser().getEffectiveAvatarUrl();
+        String botName = DiscordSRV.getPlugin().getMainGuild() != null ? DiscordSRV.getPlugin().getMainGuild().getSelfMember().getEffectiveName() : DiscordUtil.getJda().getSelfUser().getName();
+        String displayName = StringUtils.isNotBlank(player.getDisplayName()) ? MessageUtil.strip(player.getDisplayName()) : "";
+
+        TextChannel destinationChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName(channelName);
+        BiFunction<String, Boolean, String> translator = (content, needsEscape) -> {
+            if (content == null) return null;
+            content = content
+                    .replaceAll("%time%|%date%", TimeUtil.timeStamp())
+                    .replace("%username%", needsEscape ? DiscordUtil.escapeMarkdown(player.getName()) : player.getName())
+                    .replace("%displayname%", needsEscape ? DiscordUtil.escapeMarkdown(displayName) : displayName)
+                    .replace("%usernamenoescapes%", player.getName())
+                    .replace("%displaynamenoescapes%", displayName)
+                    .replace("%world%", player.getWorld().getName())
+                    .replace("%achievement%", MessageUtil.strip(needsEscape ? DiscordUtil.escapeMarkdown(finalAchievementName) : finalAchievementName))
+                    .replace("%embedavatarurl%", avatarUrl)
+                    .replace("%botavatarurl%", botAvatarUrl)
+                    .replace("%botname%", botName);
+            if (destinationChannel != null) content = DiscordUtil.translateEmotes(content, destinationChannel.getGuild());
+            content = PlaceholderUtil.replacePlaceholdersToDiscord(content, player);
+            return content;
+        };
+
+        Message discordMessage = DiscordSRV.translateMessage(messageFormat, translator);
+
+        MessageEmbed embed = discordMessage.getEmbeds().get(0);
+
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setColor(embed.getColor());
+
+        MessageEmbed.AuthorInfo authorInfo = embed.getAuthor();
+        embedBuilder.setAuthor(lastAdvancement, authorInfo.getUrl(), authorInfo.getIconUrl());
+
+        MessageBuilder messageBuilder = new MessageBuilder();
+        messageBuilder.setEmbeds(embedBuilder.build());
+
+        TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName(channelName);
+        DiscordUtil.queueMessage(textChannel, messageBuilder.build(), true);
+    }
+
 }
