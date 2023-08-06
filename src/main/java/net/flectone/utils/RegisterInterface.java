8e16a61cdf549e5b727652c9fc9c81f934598b00
Михаил Павлов <68995871+MishaNeYT@users.noreply.github.com>
2023-08-07 01:48:03 +0900

    functional interface

--- src/main/java/net/flectone/utils/RegisterInterface.java
+++ src/main/java/net/flectone/utils/RegisterInterface.java
@@ -2,6 +2,7 @@ package net.flectone.utils;
 
 import java.lang.reflect.InvocationTargetException;
 
+@FunctionalInterface
 public interface RegisterInterface {
 
     void register(Class<?> fClass) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
