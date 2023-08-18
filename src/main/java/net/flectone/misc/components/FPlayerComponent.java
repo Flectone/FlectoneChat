0c6265b7d92573250ea8e8ba38017793a0e1ee3f
TheFaser <artem.mochalin11@gmail.com>
2023-08-18 09:33:06 +0300

    FPlayerComponent: add support vault group for hover event

--- src/main/java/net/flectone/misc/components/FPlayerComponent.java
+++ src/main/java/net/flectone/misc/components/FPlayerComponent.java
@@ -1,6 +1,13 @@
 package net.flectone.misc.components;
 
+import net.flectone.integrations.vault.FVault;
+import net.flectone.managers.HookManager;
+import net.flectone.utils.ObjectUtil;
+import net.milkbowl.vault.chat.Chat;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
 
 import static net.flectone.managers.FileManager.locale;
 
@@ -10,11 +17,38 @@ public class FPlayerComponent extends FComponent{
         super(text);
 
         String playerName = sender.getName();
+        String hoverGroup = "default";
 
-        String showText = locale.getFormatString("player.hover-message", recipient, sender)
-                .replace("<player>", playerName);
+        if(HookManager.enabledVault && sender instanceof Player playerSender) {
+            Chat provider = FVault.getProvider();
 
-        addSuggestCommand("/msg " + playerName + " ");
-        addHoverText(showText);
+            String group = provider.getPrimaryGroup(playerSender);
+
+            if(group != null) {
+                hoverGroup = group;
+            }
+        }
+
+        String hoverText = ObjectUtil.formatString(getHoverString(playerName, hoverGroup, "text"),
+                recipient, sender);
+
+        addHoverText(hoverText);
+
+        String command = getHoverString(playerName, hoverGroup, "command");
+
+        switch (getHoverString(playerName, hoverGroup, "command-type").toLowerCase()) {
+            case "suggest" -> addSuggestCommand(command);
+            case "run" -> addRunCommand(command);
+        }
+    }
+
+    @NotNull
+    private String getHoverString(String playerName, String hoverGroup, String type) {
+        String hoverText = locale.getString("player.hover." + hoverGroup + "." + type);
+        hoverText = hoverText.isEmpty()
+                ? locale.getString("player.hover.default." + type)
+                : hoverText;
+
+        return hoverText.replace("<player>", playerName);
     }
 }
