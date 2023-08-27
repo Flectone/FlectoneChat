00af6d166c2ae4fe4f3da8389b23339f6efd8d63
TheFaser <artem.mochalin11@gmail.com>
2023-08-27 14:44:08 +0500

    CommandMailClear: add async complete

--- src/main/java/net/flectone/commands/CommandMailClear.java
+++ src/main/java/net/flectone/commands/CommandMailClear.java
@@ -1,11 +1,13 @@
 package net.flectone.commands;
 
+import net.flectone.Main;
 import net.flectone.managers.FPlayerManager;
 import net.flectone.misc.entity.info.Mail;
 import net.flectone.misc.commands.FCommand;
 import net.flectone.misc.commands.FTabCompleter;
 import net.flectone.misc.entity.FPlayer;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -18,32 +20,39 @@ public class CommandMailClear implements FTabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () ->
+                command(commandSender, command, s, strings));
+        return true;
+    }
 
+    private void command(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         FCommand fCommand = new FCommand(commandSender, command.getName(), s, strings);
 
-        if (fCommand.isConsoleMessage() || fCommand.isInsufficientArgs(2)) return true;
+        if (fCommand.isConsoleMessage() || fCommand.isInsufficientArgs(2)) return;
 
         String playerName = strings[0];
         FPlayer fPlayer = FPlayerManager.getPlayerFromName(playerName);
 
         if (fPlayer == null) {
             fCommand.sendMeMessage("command.null-player");
-            return true;
+            return;
         }
+
+        fPlayer.synchronizeDatabase();
 
         HashMap<UUID, Mail> mailsList = fPlayer.getMails();
 
         if (fPlayer.isOnline() || mailsList.isEmpty()) {
             fCommand.sendMeMessage("command.mail-clear.empty");
-            return true;
+            return;
         }
 
         if (!StringUtils.isNumeric(strings[1])) {
             fCommand.sendMeMessage("command.mail-clear.wrong-number");
-            return true;
+            return;
         }
 
-        if (fCommand.isHaveCD() || fCommand.isMuted()) return true;
+        if (fCommand.isHaveCD() || fCommand.isMuted()) return;
 
         int number = Integer.parseInt(strings[1]);
 
@@ -55,17 +64,18 @@ public class CommandMailClear implements FTabCompleter {
 
         if (entry == null) {
             fCommand.sendMeMessage("command.mail-clear.wrong-number");
-            return true;
+            return;
         }
 
         String[] replaceString = {"<player>", "<message>"};
         String[] replaceTo = {fPlayer.getRealName(), entry.getValue().getMessage()};
 
         fPlayer.removeMail(entry.getKey());
+        Main.getDatabase().saveMails(fPlayer, null);
 
         fCommand.sendMeMessage("command.mail-clear.message", replaceString, replaceTo);
 
-        return true;
+        return;
     }
 
     @Nullable
@@ -81,6 +91,8 @@ public class CommandMailClear implements FTabCompleter {
                 FPlayer fPlayer = FPlayerManager.getPlayerFromName(playerName);
 
                 if (fPlayer == null) break;
+
+                fPlayer.synchronizeDatabase();
 
                 HashMap<UUID, Mail> mailsList = fPlayer.getMails();
 
