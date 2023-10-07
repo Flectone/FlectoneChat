81ea50d2457c26597c0ba151dbf3e3e1ff78a584
TheFaser <artem.mochalin11@gmail.com>
2023-10-07 22:40:04 +0500

    Feature: add glass knocking

---
+++ src/main/java/net/flectone/listeners/PlayerGlassKnockingListener.java
@@ -0,0 +1,30 @@
+package net.flectone.listeners;
+
+import net.flectone.utils.ObjectUtil;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import static net.flectone.managers.FileManager.config;
+
+public class PlayerGlassKnockingListener implements Listener {
+
+    @EventHandler
+    public void onGlassKnockingEvent(PlayerInteractEvent event) {
+        if (!config.getBoolean("player.glass-knocking.enable")) return;
+        if (!event.getAction().equals(Action.LEFT_CLICK_BLOCK)) return;
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+
+        if (!player.isSneaking()
+                || block == null
+                || !(block.getType().equals(Material.GLASS) || block.getType().equals(Material.GLASS_PANE))) return;
+
+        ObjectUtil.playSound(player, "glass-knocking");
+    }
+
+}
