ee7d267645ba2e54cee485d4e9f087c0d92c7604
TheFaser <artem.mochalin11@gmail.com>
2023-11-04 18:17:07 +0500

    Integrations: add SimpleVoiceChat support

--- src/main/java/net/flectone/chat/module/integrations/FSimpleVoiceChat.java
+++ src/main/java/net/flectone/chat/module/integrations/FSimpleVoiceChat.java
@@ -6,7 +6,17 @@ import de.maxhenkel.voicechat.api.VoicechatPlugin;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
 import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent;
 import net.flectone.chat.FlectoneChat;
+import net.flectone.chat.component.FComponent;
+import net.flectone.chat.manager.FPlayerManager;
+import net.flectone.chat.model.player.FPlayer;
+import net.flectone.chat.util.MessageUtil;
+import net.flectone.chat.util.TimeUtil;
+import net.md_5.bungee.api.ChatMessageType;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import static net.flectone.chat.manager.FileManager.locale;
 
 
 public class FSimpleVoiceChat implements FIntegration, VoicechatPlugin {
@@ -18,7 +28,6 @@ public class FSimpleVoiceChat implements FIntegration, VoicechatPlugin {
     @Override
     public void init() {
         BukkitVoicechatService service = FlectoneChat.getInstance().getServer().getServicesManager().load(BukkitVoicechatService.class);
-
         if (service == null) return;
 
         service.registerPlugin(this);
@@ -60,17 +69,19 @@ public class FSimpleVoiceChat implements FIntegration, VoicechatPlugin {
     private void onMicrophonePacketEvent(@NotNull MicrophonePacketEvent event) {
         if (event.getSenderConnection() == null) return;
 
-//        Player player = (Player) event.getSenderConnection().getPlayer().getPlayer();
-//        FPlayer fPlayer = FPlayerManager.getPlayer(player);
-//
-//        if(fPlayer == null || !fPlayer.isMuted()) return;
-//
-//        event.cancel();
-//        String formatMessage = locale.getFormatString("command.mute.local-message", player)
-//                .replace("<time>", ObjectUtil.convertTimeToString(fPlayer.getMute().getDifferenceTime()))
-//                .replace("<reason>", fPlayer.getMute().getReason())
-//                .replace("<moderator>", fPlayer.getMute().getModeratorName());
-//
-//        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, FComponent.fromLegacyText(formatMessage));
+        Player player = (Player) event.getSenderConnection().getPlayer().getPlayer();
+        FPlayer fPlayer = FPlayerManager.get(player);
+        if(fPlayer == null || !fPlayer.isMuted()) return;
+
+        event.cancel();
+        String formatMessage = locale.getVaultString(player, "commands.muted")
+                .replace("<time>", TimeUtil.convertTime(player, fPlayer.getMute().getRemainingTime()))
+                .replace("<reason>", fPlayer.getMute().getReason())
+                .replace("<moderator>", fPlayer.getMute().getModeratorName());
+
+        formatMessage = formatMessage.replace(System.lineSeparator(), "");
+        formatMessage = MessageUtil.formatAll(player, formatMessage);
+
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, FComponent.fromLegacyText(formatMessage));
     }
 }
