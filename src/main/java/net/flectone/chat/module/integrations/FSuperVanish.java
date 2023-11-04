adb103052e858ee2ca73bd881bedfe2d1007cda4
TheFaser <artem.mochalin11@gmail.com>
2023-11-04 17:11:01 +0500

    Integrations: add super vanish support

--- src/main/java/net/flectone/chat/module/integrations/FSuperVanish.java
+++ src/main/java/net/flectone/chat/module/integrations/FSuperVanish.java
@@ -3,10 +3,16 @@ package net.flectone.chat.module.integrations;
 import de.myzelyam.api.vanish.PlayerHideEvent;
 import de.myzelyam.api.vanish.PlayerShowEvent;
 import net.flectone.chat.FlectoneChat;
+import net.flectone.chat.module.FModule;
+import net.flectone.chat.module.serverMessage.join.JoinModule;
+import net.flectone.chat.module.serverMessage.quit.QuitModule;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
+
+import static net.flectone.chat.manager.FileManager.locale;
 
 public class FSuperVanish implements Listener, FIntegration {
 
@@ -17,13 +23,27 @@ public class FSuperVanish implements Listener, FIntegration {
     @EventHandler
     public void onHide(@NotNull PlayerHideEvent event) {
         if (event.isCancelled()) return;
-//        QuitListener.sendQuitMessage(event.getPlayer(), "server-message.quit");
+
+        FModule fModule = FlectoneChat.getModuleManager().get(QuitModule.class);
+        if (fModule instanceof QuitModule quitModule) {
+            Player player = event.getPlayer();
+            String string = locale.getVaultString(player, "server-message.quit.message");
+            quitModule.sendAll(player, string);
+            event.setSilent(true);
+        }
     }
 
     @EventHandler
     public void onShow(@NotNull PlayerShowEvent event) {
         if (event.isCancelled()) return;
-//        JoinListener.sendJoinMessage(event.getPlayer());
+
+        FModule fModule = FlectoneChat.getModuleManager().get(JoinModule.class);
+        if (fModule instanceof JoinModule joinModule) {
+            Player player = event.getPlayer();
+            String string = locale.getVaultString(player, "server-message.join.message");
+            joinModule.sendAll(player, string);
+            event.setSilent(true);
+        }
     }
 
     @Override
