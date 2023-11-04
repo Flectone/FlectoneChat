456ebfb60b96c174d07926394768cda161d78a4f
TheFaser <artem.mochalin11@gmail.com>
2023-11-04 23:46:21 +0500

    Url: fix url hover

--- src/main/java/net/flectone/chat/component/FURLComponent.java
+++ src/main/java/net/flectone/chat/component/FURLComponent.java
@@ -1,18 +1,24 @@
 package net.flectone.chat.component;
 
+import net.flectone.chat.util.MessageUtil;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import static net.flectone.chat.manager.FileManager.config;
+import static net.flectone.chat.manager.FileManager.locale;
 
 public class FURLComponent extends FComponent{
 
     public FURLComponent(@Nullable Player sender, @NotNull Player recipient, @NotNull String text, @NotNull String url) {
         super(text);
 
-        String showText = config.getVaultString(sender, "formatting.list.url.format");
         addOpenURL(url);
-        addHoverText(showText);
+
+        if (config.getVaultBoolean(sender, "player-message.formatting.list.url.hover.enable")) {
+            String showText = locale.getVaultString(sender, "player-message.formatting.list.url.hover.message");
+            showText = MessageUtil.formatAll(sender, recipient, showText);
+            addHoverText(showText);
+        }
     }
 }
