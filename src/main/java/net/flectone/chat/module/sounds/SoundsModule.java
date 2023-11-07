0f3615e4234d8edf806121acffcb7498928a75d8
TheFaser <artem.mochalin11@gmail.com>
2023-11-07 20:04:05 +0500

    Plugin: remove debug

--- src/main/java/net/flectone/chat/module/sounds/SoundsModule.java
+++ src/main/java/net/flectone/chat/module/sounds/SoundsModule.java
@@ -2,7 +2,6 @@ package net.flectone.chat.module.sounds;
 
 import net.flectone.chat.model.sound.FSound;
 import net.flectone.chat.module.FModule;
-import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 public class SoundsModule extends FModule {
@@ -18,7 +17,6 @@ public class SoundsModule extends FModule {
     }
 
     public void play(@NotNull FSound sound) {
-        Bukkit.broadcastMessage(String.valueOf(isEnabledFor(sound.getSender())));
         if (!isEnabledFor(sound.getSender())) return;
         if (hasNoPermission(sound.getSender())) return;
         sound.play();
