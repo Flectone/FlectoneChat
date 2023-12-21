c7e78c0f40b280e5387096322a77c2cae7af9bdb
TheFaser <artem.mochalin11@gmail.com>
2023-12-22 01:41:35 +0500

    Formatting: refactor build method

--- src/main/java/net/flectone/chat/module/playerMessage/formatting/FormattingModule.java
+++ src/main/java/net/flectone/chat/module/playerMessage/formatting/FormattingModule.java
@@ -26,6 +26,8 @@ import java.util.regex.Pattern;
 
 public class FormattingModule extends FModule {
 
+    private final static Pattern MARKDOWN_URL_PATTERN = Pattern.compile("\\[(?<text>[^\\]]*)\\]\\((?<link>[^\\)]*)\\)");
+    private final static Pattern IMAGE_URL_PATTERN = Pattern.compile("^(?:https?://)?([^/]+)");
     private final static List<String> patterns = List.of("||", "**", "__", "##", "??", "~~");
     private final HashMap<String, HashMap<String, String>> FORMATTING_MAP = new HashMap<>();
 
@@ -186,37 +188,21 @@ public class FormattingModule extends FModule {
                 return wordParams;
             }
 
-            if (itemStack != null && word.equalsIgnoreCase("%item%")
-                    && formattingMap.containsKey("item")) {
-                wordParams.setItem(true);
-                wordParams.setText(itemStack.getItemMeta() != null && !itemStack.getItemMeta().getDisplayName().isEmpty()
-                        ? net.md_5.bungee.api.ChatColor.ITALIC + itemStack.getItemMeta().getDisplayName()
-                        : itemStack.getType().name());
-                return wordParams;
+            if (itemStack != null
+                    && formattingMap.containsKey("item")
+                    && word.equalsIgnoreCase("%item%")) {
+
+                return processItem(wordParams, itemStack);
             }
 
             if (word.startsWith(mentionPrefix)
-                    && formattingMap.containsKey("mention") && mentionEnabled) {
+                    && formattingMap.containsKey("mention")
+                    && mentionEnabled) {
                 String playerName = word.replaceFirst(mentionPrefix, "");
 
                 FPlayer fPlayer = playerManager.get(playerName);
                 if (fPlayer != null && fPlayer.getPlayer() != null) {
-                    Player player = fPlayer.getPlayer();
-
-                    word = config.getVaultString(sender, this + ".list.mention.format")
-                            .replace("<message>", player.getName());
-
-                    wordParams.setClickable(true);
-                    wordParams.setPlayerPingName(player.getName());
-                    wordParams.setPlayerPing(true);
-                    wordParams.setText(word);
-
-                    FModule fModule1 = moduleManager.get(SoundsModule.class);
-                    if (fModule1 instanceof SoundsModule soundsModule) {
-                        soundsModule.play(new FSound(player, "mention"));
-                    }
-
-                    return wordParams;
+                    return processMention(wordParams, sender, fPlayer.getPlayer());
                 }
             }
 
@@ -224,15 +210,10 @@ public class FormattingModule extends FModule {
 
                 if (config.getVaultBoolean(sender, this + ".list.url.markdown-support")
                         && !hasNoPermission(sender, "markdown-url")) {
-                    Matcher urlMatcher = Pattern.compile("\\[(?<text>[^\\]]*)\\]\\((?<link>[^\\)]*)\\)").matcher(word);
-                    if (urlMatcher.find()) {
-                        wordParams.setUrlText(urlMatcher.group(2));
-                        wordParams.setUrl(true);
 
-                        word = config.getVaultString(sender, this + ".list.url.format")
-                                .replace("<message>", urlMatcher.group(1));
-                        wordParams.setText(word);
-                        return wordParams;
+                    Matcher urlMatcher = MARKDOWN_URL_PATTERN.matcher(word);
+                    if (urlMatcher.find()) {
+                        return processMarkdownUrl(wordParams, sender, urlMatcher);
                     }
                 }
 
@@ -240,107 +221,43 @@ public class FormattingModule extends FModule {
                         && isUrlAllowed(sender, word)
                         && config.getVaultBoolean(sender, this + ".list.url.image.enable")
                         && !hasNoPermission(sender, "url.image")) {
+
                     FImageComponent fImageComponent = new FImageComponent(word);
                     if (fImageComponent.isCorrect()) {
-                        word = config.getVaultString(sender, this + ".list.url.image.format")
-                                .replace("<image>", fImageComponent.getText());
-                        wordParams.setText(word);
-                        wordParams.setImageComponent(fImageComponent);
-                        wordParams.setImage(true);
-                        return wordParams;
+                        return processImage(wordParams, sender, fImageComponent);
                     }
                 }
 
                 Matcher urlMatcher = Pattern.compile(formattingMap.get("url")).matcher(word);
                 if (urlMatcher.find()) {
-                    wordParams.setUrlText(word.substring(urlMatcher.start(0), urlMatcher.end(0)));
-                    wordParams.setUrl(true);
-
-                    word = config.getVaultString(sender, this + ".list.url.format")
-                            .replace("<message>", word);
-                    wordParams.setText(word);
-                    return wordParams;
+                    return processUrl(wordParams, sender, word, urlMatcher);
                 }
             }
 
             if (sender != null) {
 
-                if (formattingMap.containsKey("ping")) {
-                    String value = formattingMap.get("ping");
-                    if (value.equals(word)) {
-                        wordParams.setPing(true);
-
-                        int ping = sender.getPing();
-                        int badPing = config.getVaultInt(sender, this + ".list.ping.bad.count");
-                        int mediumPing = config.getVaultInt(sender, this + ".list.ping.medium.count");
-
-                        String pingColor;
-                        if (ping > badPing) {
-                            pingColor = config.getVaultString(sender, this + ".list.ping.bad.color");
-                        } else if (ping > mediumPing) {
-                            pingColor = config.getVaultString(sender, this + ".list.ping.medium.color");
-                        } else {
-                            pingColor = config.getVaultString(sender, this + ".list.ping.good.color");
-                        }
-
-                        word = locale.getVaultString(sender, this + ".list.ping.message")
-                                .replace("<player>", sender.getName())
-                                .replace("<ping>", pingColor + ping);
-                        wordParams.setText(word);
-
-                        return wordParams;
-                    }
+                String valuePing = formattingMap.get("ping");
+                if (valuePing != null && valuePing.equals(word)) {
+                    return processPing(wordParams, sender);
                 }
 
-                if (formattingMap.containsKey("cords")) {
-                    String value = formattingMap.get("cords");
-
-                    if (value.equals(word)) {
-                        wordParams.setCords(true);
-
-                        Location location = sender.getLocation();
-
-                        word = locale.getVaultString(sender, this + ".list.cords.message")
-                                .replace("<world>", sender.getWorld().getEnvironment().name())
-                                .replace("<biome>", location.getBlock().getBiome().name())
-                                .replace("<block_x>", String.valueOf(location.getBlockX()))
-                                .replace("<block_y>", String.valueOf(location.getBlockY()))
-                                .replace("<block_z>", String.valueOf(location.getBlockZ()));
-                        wordParams.setText(word);
-
-                        return wordParams;
-                    }
+                String valueCords = formattingMap.get("cords");
+                if (valueCords != null && valueCords.equals(word)) {
+                    return processCords(wordParams, sender);
                 }
 
-                if (formattingMap.containsKey("stats")) {
-                    String value = formattingMap.get("stats");
-                    if (value.equals(word)) {
-                        wordParams.setStats(true);
-
-                        AttributeInstance armor = sender.getAttribute(Attribute.GENERIC_ARMOR);
-                        AttributeInstance damage = sender.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
-
-                        word = locale.getVaultString(sender, this + ".list.stats.message")
-                                .replace("<hp>", String.valueOf(Math.round(sender.getHealth() * 10.0)/10.0))
-                                .replace("<armor>", String.valueOf(armor != null ? Math.round(armor.getValue() * 10.0)/10.0 : 0.0))
-                                .replace("<exp>", sender.getLevel() + ".0")
-                                .replace("<food>", sender.getFoodLevel() + ".0")
-                                .replace("<attack>", String.valueOf(damage != null ? Math.round(damage.getValue() * 10.0)/10.0 : 0.0));
-
-                        wordParams.setText(word);
-                        return wordParams;
-                    }
+                String valueStats = formattingMap.get("stats");
+                if (valueStats != null && valueStats.equals(word)) {
+                    return processStats(wordParams, sender);
                 }
             }
 
             wordParams.addParameters(textParameters.getParameters());
 
             if (textParameters.contains("||")) {
-                wordParams.setHide(true);
-
-                wordParams.setHideMessage(config.getVaultString(sender, this + ".list.markdown-||.symbol")
-                        .repeat(ChatColor.stripColor(word).length()));
+                processMarkdownHide(wordParams, sender, word);
             }
+
             wordParams.setText(word);
             return wordParams;
         }).forEachOrdered(wordParams -> {
@@ -357,10 +274,127 @@ public class FormattingModule extends FModule {
         }
     }
 
-    public boolean isUrlAllowed(@Nullable CommandSender player, @NotNull String urlString) {
-        Pattern pattern = Pattern.compile("^(?:https?://)?([^/]+)");
-        Matcher matcher = pattern.matcher(urlString);
+    public WordParams processItem(@NotNull WordParams wordParams, @NotNull ItemStack itemStack) {
+        wordParams.setItem(true);
+        wordParams.setText(itemStack.getItemMeta() != null && !itemStack.getItemMeta().getDisplayName().isEmpty()
+                ? net.md_5.bungee.api.ChatColor.ITALIC + itemStack.getItemMeta().getDisplayName()
+                : itemStack.getType().name());
+        return wordParams;
+    }
 
+    public WordParams processMention(@NotNull WordParams wordParams, @Nullable CommandSender sender, @NotNull Player player) {
+        String word = config.getVaultString(sender, this + ".list.mention.format")
+                .replace("<message>", player.getName());
+
+        wordParams.setClickable(true);
+        wordParams.setPlayerPingName(player.getName());
+        wordParams.setPlayerPing(true);
+        wordParams.setText(word);
+
+        FModule fModule1 = moduleManager.get(SoundsModule.class);
+        if (fModule1 instanceof SoundsModule soundsModule) {
+            soundsModule.play(new FSound(player, "mention"));
+        }
+
+        return wordParams;
+    }
+
+    public WordParams processMarkdownUrl(@NotNull WordParams wordParams, @Nullable CommandSender sender, @NotNull Matcher urlMatcher) {
+        wordParams.setUrlText(urlMatcher.group(2));
+        wordParams.setUrl(true);
+
+        String word = config.getVaultString(sender, this + ".list.url.format")
+                .replace("<message>", urlMatcher.group(1));
+        wordParams.setText(word);
+        return wordParams;
+    }
+
+    public WordParams processImage(@NotNull WordParams wordParams, @Nullable CommandSender sender, @NotNull FImageComponent fImageComponent) {
+        String word = config.getVaultString(sender, this + ".list.url.image.format")
+                .replace("<image>", fImageComponent.getText());
+        wordParams.setText(word);
+        wordParams.setImageComponent(fImageComponent);
+        wordParams.setImage(true);
+        return wordParams;
+    }
+
+    public WordParams processUrl(@NotNull WordParams wordParams, @Nullable CommandSender sender, @NotNull String word, @NotNull Matcher urlMatcher) {
+        wordParams.setUrlText(word.substring(urlMatcher.start(0), urlMatcher.end(0)));
+        wordParams.setUrl(true);
+
+        word = config.getVaultString(sender, this + ".list.url.format")
+                .replace("<message>", word);
+        wordParams.setText(word);
+        return wordParams;
+    }
+
+    public WordParams processPing(@NotNull WordParams wordParams, @NotNull Player sender) {
+        wordParams.setPing(true);
+
+        int ping = sender.getPing();
+        int badPing = config.getVaultInt(sender, this + ".list.ping.bad.count");
+        int mediumPing = config.getVaultInt(sender, this + ".list.ping.medium.count");
+
+        String pingColor;
+        if (ping > badPing) {
+            pingColor = config.getVaultString(sender, this + ".list.ping.bad.color");
+        } else if (ping > mediumPing) {
+            pingColor = config.getVaultString(sender, this + ".list.ping.medium.color");
+        } else {
+            pingColor = config.getVaultString(sender, this + ".list.ping.good.color");
+        }
+
+        String word = locale.getVaultString(sender, this + ".list.ping.message")
+                .replace("<player>", sender.getName())
+                .replace("<ping>", pingColor + ping);
+        wordParams.setText(word);
+
+        return wordParams;
+    }
+
+    public WordParams processCords(@NotNull WordParams wordParams, @NotNull Player sender) {
+        wordParams.setCords(true);
+
+        Location location = sender.getLocation();
+
+        String word = locale.getVaultString(sender, this + ".list.cords.message")
+                .replace("<world>", sender.getWorld().getEnvironment().name())
+                .replace("<biome>", location.getBlock().getBiome().name())
+                .replace("<block_x>", String.valueOf(location.getBlockX()))
+                .replace("<block_y>", String.valueOf(location.getBlockY()))
+                .replace("<block_z>", String.valueOf(location.getBlockZ()));
+        wordParams.setText(word);
+
+        return wordParams;
+    }
+
+    public WordParams processStats(@NotNull WordParams wordParams, @NotNull Player sender) {
+        wordParams.setStats(true);
+
+        AttributeInstance armor = sender.getAttribute(Attribute.GENERIC_ARMOR);
+        AttributeInstance damage = sender.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
+
+        String word = locale.getVaultString(sender, this + ".list.stats.message")
+                .replace("<hp>", String.valueOf(Math.round(sender.getHealth() * 10.0)/10.0))
+                .replace("<armor>", String.valueOf(armor != null ? Math.round(armor.getValue() * 10.0)/10.0 : 0.0))
+                .replace("<exp>", sender.getLevel() + ".0")
+                .replace("<food>", sender.getFoodLevel() + ".0")
+                .replace("<attack>", String.valueOf(damage != null ? Math.round(damage.getValue() * 10.0)/10.0 : 0.0));
+
+        wordParams.setText(word);
+        return wordParams;
+    }
+
+    public WordParams processMarkdownHide(@NotNull WordParams wordParams, @Nullable CommandSender sender, @NotNull String word) {
+        wordParams.setHide(true);
+
+        wordParams.setHideMessage(config.getVaultString(sender, this + ".list.markdown-||.symbol")
+                .repeat(ChatColor.stripColor(word).length()));
+        return wordParams;
+    }
+
+    public boolean isUrlAllowed(@Nullable CommandSender player, @NotNull String urlString) {
+        Matcher matcher = IMAGE_URL_PATTERN.matcher(urlString);
         if (matcher.find()) {
             String host = matcher.group(1).toLowerCase();
             return FlectoneChat.getPlugin().getFileManager()
